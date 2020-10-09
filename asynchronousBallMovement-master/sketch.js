df4ddae75adb3b9e24eb281e1919a1886d80800b
ParshvaJogani <69082393+ParshvaJogani@users.noreply.github.com>
2020-10-09 20:53:08 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,41 @@
+var ball;
+var db,pos;
+
+function setup(){
+    createCanvas(500,500);
+    db = firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    db.ref("ball/position").on("value",setPosition);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function setPosition(data){
+pos = data.val();
+ball.x = pos.x;
+ball.y = pos.y
+}
+
+function changePosition(x,y){
+   db.ref("ball/position").set({
+       x:pos.x+x,
+       y:pos.y+y
+   })
+}
